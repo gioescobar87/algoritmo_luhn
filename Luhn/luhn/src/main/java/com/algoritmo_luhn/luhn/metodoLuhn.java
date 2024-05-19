d428f9232196d10654dd9b97c974efb246b20a8c
gioescobar87 <descobarl4@miumg.edu.gt>
2024-05-19 15:19:45 -0600

    Despliegue de operación realizada

--- Luhn/luhn/src/main/java/com/algoritmo_luhn/luhn/metodoLuhn.java
+++ Luhn/luhn/src/main/java/com/algoritmo_luhn/luhn/metodoLuhn.java
@@ -7,16 +7,17 @@ import javax.swing.JOptionPane;
 public class metodoLuhn {
 
     Stack <Integer> pilaInvertidaOriginal = new Stack();
-    
+    String operacion = "";
+
 
     public boolean parsear(char cadena[]){
+        System.out.println(cadena);
         invertirOrden(cadena);
         return doblarSegundosDigitos();
     }
 
     private void invertirOrden(char cadena[]){
         for (char c : cadena) {
-            System.out.println("valor en cadena: "+c);
             pilaInvertidaOriginal.push(c-'0');
         }
     }
@@ -29,20 +30,23 @@ public class metodoLuhn {
             if (indice%2==1) {
                 int numeroDoble = pilaInvertidaOriginal.peek()*2;
                 if (numeroDoble>9) {
-                    nuevoNumero = 1+ (numeroDoble%10);
+                    int segundoDigito = numeroDoble%10;
+                    nuevoNumero = 1+ (segundoDigito);
                     sumaMultiplo10+=nuevoNumero;
+                    operacion+="+(1+"+numeroDoble%10+")";
                 }else{
                     sumaMultiplo10+=numeroDoble;
+                    operacion+="+"+numeroDoble;
                 }
             }else{
                 sumaMultiplo10+=pilaInvertidaOriginal.peek();
-                pilaInvertidaOriginal.peek();
+                operacion+= (indice==0 ? "" : "+" )+pilaInvertidaOriginal.peek();
             }
             indice++;
             pilaInvertidaOriginal.pop();
         }
 
-        JOptionPane.showMessageDialog(null, sumaMultiplo10);
+        JOptionPane.showMessageDialog(null, "Resultado: "+operacion+" = "+sumaMultiplo10);
 
         if (sumaMultiplo10%10==0) {
             return true;
